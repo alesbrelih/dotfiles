3b2822754ea075ef18debba1d36228f8947c7999
Ales Brelih <ales.brelih@3fs.si>
2024-05-28 17:17:04 +0200

    feat: ability to toggle conform format on save

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,3 +1,15 @@
+vim.api.nvim_create_user_command('ConformToggle', function(args)
+  if args.bang then
+    -- ConformToggle! will disable formatting just for this buffer
+    vim.b.disable_autoformat = not (vim.b.disable_autoformat or false)
+  else
+    vim.g.disable_autoformat = not (vim.g.disable_autoformat or false)
+  end
+end, {
+  desc = 'Conform toggle autoformat-on-save',
+  bang = true,
+})
+
 return {
   'stevearc/conform.nvim',
   lazy = false,
@@ -10,10 +22,29 @@ return {
       mode = '',
       desc = '[F]ormat buffer',
     },
+    {
+      '<leader>ufa',
+      '<cmd>ConformToggle<cr>',
+      mode = '',
+      desc = '[U]I auto[F]ormat toggle [A]ll buffers',
+    },
+    {
+      '<leader>ufc',
+      '<cmd>ConformToggle!<cr>',
+      mode = '',
+      desc = '[U]I auto[F]ormat toggle [C]urrent buffer',
+    },
   },
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
+      -- add ability to disable autoformat
+      print(vim.inspect(vim.g.disable_autoformat))
+      print(vim.inspect(vim.b[bufnr].disable_autoformat))
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
