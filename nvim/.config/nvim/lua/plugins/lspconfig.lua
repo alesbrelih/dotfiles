a3e489f61dc2359f5e273d77e41b71798e9b5b00
Ales Brelih <ales.brelih@3fs.si>
2024-08-16 15:34:22 +0200

    fix: which-key update

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -176,42 +176,6 @@ return {
           },
         },
       },
-      tsserver = {
-        keys = {
-          {
-            '<leader>co',
-            function()
-              vim.lsp.buf.code_action {
-                apply = true,
-                context = {
-                  only = { 'source.organizeImports.ts' },
-                  diagnostics = {},
-                },
-              }
-            end,
-            desc = 'Organize Imports',
-          },
-          {
-            '<leader>cR',
-            function()
-              vim.lsp.buf.code_action {
-                apply = true,
-                context = {
-                  only = { 'source.removeUnused.ts' },
-                  diagnostics = {},
-                },
-              }
-            end,
-            desc = 'Remove Unused Imports',
-          },
-        },
-        ---@diagnostic disable-next-line: missing-fields
-        settings = {
-          completions = {
-            completeFunctionCalls = true,
-          },
-        },
-      },
       lua_ls = {
         settings = {
           Lua = {
@@ -221,6 +185,10 @@ return {
             hint = { enable = true },
           },
         },
+      },
+      angularls = {},
+      emmet_ls = {
+        filetypes = { 'html' },
       },
     }
 
@@ -237,7 +205,7 @@ return {
       'marksman',
       'codelldb',
       'gitlab-ci-ls',
-      'tsserver',
+      'vtsls',
     })
 
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
